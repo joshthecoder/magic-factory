aac71049ca5f8d394ee73496e70004d62702690b
Josh Roesslein <jroesslein@gmail.com>
2014-10-18 19:06:51 -0700

    Add card model and db migration.

---
+++ db/migrate/20141019003455_create_cards.rb
@@ -0,0 +1,22 @@
+class CreateCards < ActiveRecord::Migration
+  def change
+    create_table :cards do |t|
+      t.column :name, :string, null: false
+      t.column :type, :string, null: false
+      t.column :names, :string
+      t.column :colors, :string
+      t.column :layout, :integer, null: false
+      t.column :mana_cost, :string
+      t.column :converted_mana_cost, :integer, null: false
+      t.column :rarity, :string, null: false
+      t.column :card_number, :string, null: false
+      t.column :power, :string
+      t.column :toughness, :string
+      t.column :loyalty, :integer
+      t.column :image_name, :string
+      t.column :release_date, :date, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
