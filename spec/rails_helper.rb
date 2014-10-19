77c81511c08906cb57c11d87eb7081b0225d90f7
Josh Roesslein <jroesslein@gmail.com>
2014-10-19 13:08:51 -0700

    Install database cleaner.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require 'factory_helper'
+require 'database_cleaner_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
